d9415fc0c64ab3eecf7bcb1c0953a2126151261d
snt4589 <tripathisavitanandan@gmail.com>
2019-02-28 05:07:45 +0530

    added LoginActivity and recyclerView and project view

--- app/src/main/java/mk/snt/accord/activities/MainActivity.java
+++ app/src/main/java/mk/snt/accord/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package mk.snt.accord.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +19,8 @@ import mk.snt.accord.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    Intent intent = getIntent();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
